3c7d668c0f72c37b641d7e8d1cd8c1e866916a4f
cpmooney <christopher.mooney.math@gmail.com>
2023-11-02 05:18:12 -0500

    Daisy

--- app/card-component.tsx
+++ app/card-component.tsx
@@ -4,10 +4,15 @@ export default function CardComponent(props: CardComponentProps) {
   const {showAnswer, card} = props;
   const answer = showAnswer ? card.fullCitation : "";
   return (
-    <>
-      <div>{card.fragment}</div>
-      <div>{answer}</div>
-    </>
+    <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body">
+    <h2 className="card-title">{card.fragment}</h2>
+    <p>{card.fullCitation}</p>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
   );
 }
 
