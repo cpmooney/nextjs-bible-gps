110cd156c00a61c0581e991ed7ef077dfdca3064
Chris Mooney <christopher.mooney.math@gmail.com>
2024-01-01 17:23:22 -0600

    Returning loading page to its original state

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader } from "./components/loader-component";
 export default function Loading() {
     return (
       <main className="text-center">  
-      laskdfjalsdkfj alsdkfj 
+      <Loader />
       </main>
     )
   }
