b0ba1d1cfcad8f13b686964ebb5245ea53642147
cpmooney <christopher.mooney.math@gmail.com>
2024-01-02 20:01:08 -0600

    Getting some functionality

--- app/components/additional-actions.tsx
+++ app/components/additional-actions.tsx
@@ -4,6 +4,7 @@ import {
   ChartBarIcon,
   InformationCircleIcon,
   LightBulbIcon,
+  ListBulletIcon,
   PencilIcon,
   PlusCircleIcon,
 } from "@heroicons/react/24/outline";
@@ -21,6 +22,7 @@ export const AdditionalActionComponents = () => {
   const createPartialCitation = () => showModal("create_partial_citation");
   const editCitation = () => router.push(editUrl(obtainCurrentCard().id));
   const addNewCitation = () => router.push("/partial-list");
+  const gotoList = () => router.push("/list");
 
   return (
     <>
@@ -50,8 +52,10 @@ export const AdditionalActionComponents = () => {
           >
             <LightBulbIcon className="w-6" />
           </button>
-          <button className="btn btn-btnPrimary mr-2 mt-2 mb-2">
-            <ChartBarIcon className="w-6" />
+          <button className="btn btn-btnPrimary mr-2 mt-2 mb-2"
+          onClick={gotoList}
+          >
+            <ListBulletIcon className="w-6" />
           </button>
         </div>
       </div>
--- app/list/components/citation.tsx
+++ app/list/components/citation.tsx
@@ -2,12 +2,32 @@
 
 import { Citation } from "@/models/citation";
 import { buildFullCitation } from "@/utilities/additional-citation-methods";
+import { PencilIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/navigation";
 
 interface Props {
     citation: Citation;
 }
 
 export default function CitationDisplay({citation}: Props) {
+    const router = useRouter();
     const fullCitation = buildFullCitation(citation);
-    return <div>{fullCitation}</div>
+    const editCard = () => {
+        router.push(`/edit/${citation.id}`);
+    };
+    
+    return <div>
+    <div className="h-5 w-64 btn btn-btnPrimary bg-green-400 ml-2 mt-2">
+        {fullCitation}
+    </div>
+    <div className="h-5 w-64 btn btn-btnPrimary bg-green-400 ml-2 mt-2">
+        {citation.fragment}
+    </div>
+    <button
+        className="h-5 flex-auto btn btn-btnPrimary bg-green-400 ml-2 mt-2"
+        onClick={editCard}
+    >
+        <PencilIcon className="h-5 w-5" />
+    </button>
+    </div>;
 }
