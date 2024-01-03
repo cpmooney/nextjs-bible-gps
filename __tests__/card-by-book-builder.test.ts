10b700b7f733cebc3ea11f3c1c811be09edc2ea2
cpmooney <christopher.mooney.math@gmail.com>
2024-01-02 19:16:53 -0600

    Fixing tests

--- __tests__/card-by-book-builder.test.ts
+++ __tests__/card-by-book-builder.test.ts
@@ -2,33 +2,46 @@ import { Citation } from "@/models/citation";
 import { buildCardsByBook } from "@/utilities/card-by-book-builder";
 
 describe("buildCardsByBook", () => {
-    it("orders the citations", () => {
+    describe("organization by book", () => {
         const cardsByBook = buildCardsByBook([
-            makeCitation("Acts", 5, 7),
-            makeCitation("Romans", 5, 7),
-            makeCitation("Acts", 7, 2),
+            makeCitation("Acts", 16, 2),
             makeCitation("Romans", 7, 2),
-            makeCitation("Acts", 5, 1),
-            makeCitation("Romans", 5, 1),
+            makeCitation("Acts", 15, 3),
         ]);
+        
+        it("puts citations with the same book in the same entry", () => {
+            expect(cardsByBook[0].book).toEqual("Acts");
+            expect(cardsByBook[0].cards.length).toEqual(2);
+        });
+        
+        it("puts citations with different books in different entries", () => {
+            expect(cardsByBook[1].book).toEqual("Romans");
+            expect(cardsByBook[1].cards.length).toEqual(1);
+        });
+    });
+    
+    it("sorts citations by chapter first", () => {
+        const cardsByBook = buildCardsByBook([
+            makeCitation("Acts", 16, 2),
+            makeCitation("Acts", 15, 3),
+            makeCitation("Acts", 15, 2)
+        ]);
+        
+        expect(cardsByBook[0].cards[0].chapter).toEqual(15);
+        expect(cardsByBook[0].cards[1].chapter).toEqual(15);
+        expect(cardsByBook[0].cards[2].chapter).toEqual(16);
+    });
 
-        expect(cardsByBook.length).toEqual(2);
-        expect(cardsByBook[0].book).toEqual("Acts");
-        expect(cardsByBook[0].cards.length).toEqual(3);
-        expect(cardsByBook[0].cards[0].chapter).toEqual(5);
-        expect(cardsByBook[0].cards[0].firstVerse).toEqual(1);
-        expect(cardsByBook[0].cards[1].chapter).toEqual(5);
-        expect(cardsByBook[0].cards[1].firstVerse).toEqual(7);
-        expect(cardsByBook[0].cards[1].chapter).toEqual(7);
-        expect(cardsByBook[0].cards[1].firstVerse).toEqual(2);
-        expect(cardsByBook[1].book).toEqual("Romans");
-        expect(cardsByBook[1].cards.length).toEqual(3);
-        expect(cardsByBook[1].cards[0].chapter).toEqual(5);
-        expect(cardsByBook[1].cards[0].firstVerse).toEqual(1);
-        expect(cardsByBook[1].cards[1].chapter).toEqual(5);
-        expect(cardsByBook[1].cards[1].firstVerse).toEqual(7);
-        expect(cardsByBook[1].cards[1].chapter).toEqual(7);
-        expect(cardsByBook[1].cards[1].firstVerse).toEqual(2);
+    it("sorts citations by verse second", () => {
+        const cardsByBook = buildCardsByBook([
+            makeCitation("Acts", 16, 2),
+            makeCitation("Acts", 15, 3),
+            makeCitation("Acts", 15, 2)
+        ]);
+        
+        expect(cardsByBook[0].cards[0].firstVerse).toEqual(2);
+        expect(cardsByBook[0].cards[1].firstVerse).toEqual(3);
+        expect(cardsByBook[0].cards[2].firstVerse).toEqual(2);
     });
 });
 
@@ -36,10 +49,10 @@ const makeCitation = (book: string, chapter: number, firstVerse: number): Citati
     book,
     chapter,
     firstVerse,
-                fragment: "",
-                active: true,
-                entire: "",
-                score: 0,
-                suffix: "",
-                tags: []
+    fragment: "",
+    active: true,
+    entire: "",
+    score: 0,
+    suffix: "",
+    tags: []
 });
--- src/utilities/card-by-book-builder.ts
+++ src/utilities/card-by-book-builder.ts
@@ -31,13 +31,12 @@ const insertCardInOrder = (cards: Citation[], card: Citation): void => {
     } else {
       cards.splice(index, 0, card);
     }
-    cards.map((c) => console.log(`  ${c.book} ${c.chapter}:${c.firstVerse}`));
   }
 
 const compareCards = (card1: Citation, card2: Citation): number => {
   if (card2.chapter !== card1.chapter) {
-    return card2.chapter - card1.chapter;
+    return card1.chapter - card1.chapter;
   } else {
-    return card2.firstVerse - card1.firstVerse;
+    return card1.firstVerse - card1.firstVerse;
   }
 };
