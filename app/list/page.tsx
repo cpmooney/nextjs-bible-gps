a9d62b867f1a59017bccfdcc8dad93c9ac3568c3
Chris Mooney <christopher.mooney.math@gmail.com>
2024-01-04 18:55:59 -0600

    Fix book activation

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -5,19 +5,19 @@ import {toKebabCase} from "@/utilities/misc";
 import {useDeckStateContext} from "app/components/providers/deck-state-provider";
 import {useSearchParams} from "next/navigation";
 import {Book} from "./components/book";
+import { Citation } from "@/models/citation";
 
 export default function CardListPage() {
   const searchParams = useSearchParams();
   const {obtainCardsByBook, obtainCardById} = useDeckStateContext();
   const id = searchParams?.get("id");
-  const citationToActivate: number | undefined = obtainCardById(
-    parseInt(id ?? "0")
-  );
+  const citationToActivate: Citation | undefined = id ?
+    obtainCardById(parseInt(id)) : undefined;
 
   const cardsByBook: OrderedCardsByBook = obtainCardsByBook();
   return cardsByBook.map(({book, cards}) => {
     return (
-      <Book key={toKebabCase(book)} book={book} citations={cards} active />
+      <Book key={toKebabCase(book)} book={book} citations={cards} active={book == citationToActivate?.book} />
     );
   });
 }
