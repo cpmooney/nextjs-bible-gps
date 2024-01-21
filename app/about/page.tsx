c3e8fb51dc48f4809e988bf9f4ae8a9b5fea2ef0
Chris Mooney <christopher.mooney.math@gmail.com>
2024-01-20 19:50:05 -0600

    Adding content

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+  // TODO: Find a better way to manage content so we don't need to worry about escaping, rendering
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <div className="card-body">
+        <h2 className="text-center text-2xl h-16 mb-2">About</h2>
+      </div>
+    </div>
+  );
+}
--- app/getting-started/components/lets-go.tsx
+++ app/getting-started/components/lets-go.tsx
@@ -2,9 +2,11 @@
 
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 import { duplicateDemoCards } from "app/actions";
+import { useDeckStateContext } from "app/components/providers/deck-state-provider";
 import { useRouter } from "next/navigation";
 
 export default function LetsGo() {
+  const { userHasNoCards } = useDeckStateContext();
   const router = useRouter();
   const letsGo = async () => {
     await duplicateDemoCards();
@@ -14,6 +16,7 @@ export default function LetsGo() {
     <button
       className="flex-1 btn btn-btnPrimary enabled:bg-green-400 h-20"
       onClick={letsGo}
+      disabled={!userHasNoCards()}
     >
       <CheckCircleIcon className="h-10 w-10 mr-2" />
     </button>
