384223fbb6a1a7eff597a8dda1d9890579776473
Chris Mooney <christopher.mooney.math@gmail.com>
2024-02-10 22:04:10 -0600

    Moving View Citation button down

--- app/page.tsx
+++ app/page.tsx
@@ -10,17 +10,17 @@ export default function Home() {
     <CardStateProvider>
       <div className="p-4">
         <div className="w-full max-w-2xl bg-white shadow-xl mb-4 p-4">
-          <div className="mt-2">
-            <InfoButton />
-          </div>
-        </div>
-        <div className="w-full max-w-2xl bg-white shadow-xl mb-4 p-4">
           <Label title="Guess the citation for this passage" />
           <Content />
         </div>
         <div className="w-full max-w-2xl bg-white shadow-xl mb-4 p-4">
           <Label title="How did you do?" />
           <ResponseButtons />
+        </div>
+        <div className="w-full max-w-2xl bg-white shadow-xl mb-4 p-4">
+          <div className="mt-2">
+            <InfoButton />
+          </div>
         </div>
         <div className="w-full max-w-2xl bg-white shadow-xl mb-4 p-4">
           <Score />
