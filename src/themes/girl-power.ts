51ec4ea7bf14d5e4c2f075045290e8a81802e63a
Chris Mooney <89551217+cpmooney@users.noreply.github.com>
2024-02-19 12:27:36 -0600

    Implementing theme changer

--- src/themes/girl-power.ts
+++ src/themes/girl-power.ts
@@ -1,10 +1,9 @@
 import base from "./base";
-import { extend } from "./utils";
+import {extend} from "./utils";
 
 export default extend(base, {
-    primary: "#F2E205",
-    secondary: "#F205E2",
-    darkPrimary: "#2F362E",
-    lightPrimary: "#F2EBE4",
-    white: "#FFFFFF",
-})
+  primary: "#39A6A3",
+  secondary: "#F56273",
+  darkPrimary: "#283561",
+  lightPrimary: "#FFF1E8",
+});
--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,8 +1,24 @@
 import base from "./base";
-import { ThemeColorDictionary } from "./utils";
+import girlPower from "./girl-power";
+import {ThemeColorDictionary} from "./utils";
 
-export const DEFAULT_THEME: string = 'base';
+export const DEFAULT_THEME: string = "base";
 
 export const themes: ThemeColorDictionary = {
-    base
+  base,
+  girlPower,
 };
+
+interface Option {
+  name: string;
+  themeName: string;
+}
+
+const camelCaseToReadable = (str: string): string => {
+  return str.replace(/([A-Z])/g, " $1").replace(/^./, (s) => s.toUpperCase());
+};
+
+export const ThemeOptions: Option[] = Object.keys(themes).map((themeName) => ({
+  name: camelCaseToReadable(themeName),
+  themeName,
+}));
